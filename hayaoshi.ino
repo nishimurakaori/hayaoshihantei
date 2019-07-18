01a0a87cf6e80781bc9efdaa9e11590386471eef
NishimuraKaori <usakoyama@gmail.com>
2019-07-18 14:58:46 +0900

    add maru sound(tone())

--- hayaoshi.ino
+++ hayaoshi.ino
@@ -1,18 +1,30 @@
+#include <Arduino.h>
+/*#include "./audio_player.hpp"*/
+
+#define BEATTIME 100 //音を出している時間(msec)
+#define SPEAKER 3 //スピーカーの出力ピン番号
+
+
 bool ring_buffer[5] = {false};
 static const int MAX_RING_BUFFER = sizeof(ring_buffer) / sizeof(ring_buffer[0]);
 int ring_buffer_index;
 
-static const int MARU = 0;
-static const int BATSU = 0;
+/*
+AudioPlayer *g_player;
+byte g_playerInstance[sizeof(AudioPlayer)];
+*/
+
+static const int MARU = 1;
+static const int BATSU = 2;
 static const int RESET = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
-//Serial.begin(9600);
+Serial.begin(9600);
   
-  pinMode(3, INPUT);
-  pinMode(2, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+  //pinMode(3, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   ring_buffer_index = 0;
 
@@ -40,18 +52,53 @@ bool IsPush()
 void loop() {
 
   // put your main code here, to run repeatedly:
-  const bool val = digitalRead(3) == HIGH;
+//  const bool val = digitalRead(2) == HIGH;
+  const bool val = digitalRead(2) == LOW;
+
+  Serial.println(val);
+  
   PushBuffer(val);
   const int is_push = IsPush() ? HIGH : LOW;
 
 //  Serial.println(val);
+ // Serial.println((int)IsPush);
   
   if(is_push){
-    digitalWrite(2, LOW);
+//    digitalWrite(2, LOW);
     digitalWrite(LED_BUILTIN, LOW);
+//    g_player->play();
+
+/*
+    tone(SPEAKER,392,BEATTIME) ; // ソ
+    delay(BEATTIME) ;
+    tone(SPEAKER,330,BEATTIME) ; // ミ
+    delay(BEATTIME) ;
+    tone(SPEAKER,392,BEATTIME) ; // ソ
+    delay(BEATTIME) ;
+    tone(SPEAKER,330,BEATTIME) ; // ミ
+    delay(BEATTIME) ;
+    tone(SPEAKER,392,BEATTIME) ; // ソ
+    delay(BEATTIME) ;
+    tone(SPEAKER,330,500) ; // ミ
+    delay(500) ;
+ //   delay(300) ;
+*/
+    tone(SPEAKER,659,BEATTIME) ; // ミ
+    delay(BEATTIME) ;
+    tone(SPEAKER,523,BEATTIME) ; // ド
+    delay(BEATTIME) ;
+    tone(SPEAKER,659,BEATTIME) ; // ミ
+    delay(BEATTIME) ;
+    tone(SPEAKER,523,BEATTIME) ; // ド
+    delay(BEATTIME) ;
+    tone(SPEAKER,659,BEATTIME) ; // ミ
+    delay(BEATTIME) ;
+    tone(SPEAKER,523,500) ; // ド
+    delay(500) ;
+ 
   }
   else {
-    digitalWrite(2, HIGH);
+  //  digitalWrite(2, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
   }
   delay(1);
