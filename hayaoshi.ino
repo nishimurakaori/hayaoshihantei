e86cc6e7c37d4d03b5509cc4243540334054dd2a
NishimuraKaori <usakoyama@gmail.com>
2019-07-19 11:43:25 +0900

    add batsu, reset

--- hayaoshi.ino
+++ hayaoshi.ino
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-/*#include "./audio_player.hpp"*/
 
 #define BEATTIME 100 //音を出している時間(msec)
 #define SPEAKER 3 //スピーカーの出力ピン番号
@@ -10,7 +9,6 @@
 
 #define RESET_OUTPUT 2
 
-
 #define MARU 0
 #define BATSU 1
 #define RESET 2
@@ -19,37 +17,31 @@ bool ring_buffer[3][5] = {false};
 static const int MAX_RING_BUFFER = sizeof(ring_buffer[0]) / sizeof(ring_buffer[0][0]);
 int ring_buffer_index[3];
 
-/*
-AudioPlayer *g_player;
-byte g_playerInstance[sizeof(AudioPlayer)];
-*/
-
-
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
   
   pinMode(MARU_INPUT, INPUT_PULLUP);
+  pinMode(BATSU_INPUT, INPUT_PULLUP);
+  pinMode(RESET_INPUT, INPUT_PULLUP);
   
-  //pinMode(3, OUTPUT);
+  //pinMode(RESET_OUTPUT, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   ring_buffer_index[MARU] = 0;
   ring_buffer_index[BATSU] = 0;
   ring_buffer_index[RESET] = 0;
-
 }
 
 void PushBuffer(int button, bool v)
 {
-  
   ring_buffer[button][ring_buffer_index[button]] = v;
   ring_buffer_index[button] = (ring_buffer_index[button] + 1) % MAX_RING_BUFFER;
 
   for(int i = 0; i < MAX_RING_BUFFER; i++){
-    Serial.print(ring_buffer[MARU][i]);
+    //Serial.print(ring_buffer[BATSU][i]);
   }
-  Serial.println();
+  //Serial.println();
 }
 
 bool IsPush(int button)
@@ -70,21 +62,25 @@ void loop() {
   // put your main code here, to run repeatedly:
 //  const bool val = digitalRead(2) == HIGH;
   const bool val_maru = digitalRead(MARU_INPUT) == LOW;
+  const bool val_batsu = digitalRead(BATSU_INPUT) == LOW;
+  const bool val_reset = digitalRead(RESET_INPUT) == LOW;
 
-  //Serial.println(val_maru);
+  //Serial.println(val_batsu);
   
   PushBuffer(MARU, val_maru);
+  PushBuffer(BATSU, val_batsu);
+  PushBuffer(RESET, val_reset);
  // const int is_push = IsPush() ? HIGH : LOW;
   int is_push[3];
   is_push[MARU] = IsPush(MARU) ? HIGH : LOW;
+  is_push[BATSU] = IsPush(BATSU) ? HIGH : LOW;
+  is_push[RESET] = IsPush(RESET) ? HIGH : LOW;
   
-  //Serial.println(is_push[MARU]);
+  //Serial.println(is_push[BATSU]);
   
   if(is_push[MARU]){
-//    digitalWrite(2, LOW);
+    digitalWrite(RESET_OUTPUT, LOW);
     digitalWrite(LED_BUILTIN, LOW);
-//    g_player->play();
-
 
     tone(SPEAKER,784,BEATTIME) ; // ソ
     delay(BEATTIME) ;
@@ -98,25 +94,20 @@ void loop() {
     delay(BEATTIME) ;
     tone(SPEAKER,659,500) ; // ミ
     delay(500) ;
- //   delay(300) ;
-
-/*
-    tone(SPEAKER,659,BEATTIME) ; // ミ
-    delay(BEATTIME) ;
-    tone(SPEAKER,523,BEATTIME) ; // ド
-    delay(BEATTIME) ;
-    tone(SPEAKER,659,BEATTIME) ; // ミ
-    delay(BEATTIME) ;
-    tone(SPEAKER,523,BEATTIME) ; // ド
-    delay(BEATTIME) ;
-    tone(SPEAKER,659,BEATTIME) ; // ミ
-    delay(BEATTIME) ;
-    tone(SPEAKER,523,500) ; // ド
-    delay(500) ;
- */
   }
+  
+  else if (is_push[BATSU]){
+    digitalWrite(RESET_OUTPUT, LOW);
+    tone(SPEAKER,277,200) ; // ド＃
+    delay(200) ;
+
+  }
+  else if (is_push[RESET]){
+    digitalWrite(RESET_OUTPUT, LOW);
+  }
+
   else {
-  //  digitalWrite(2, HIGH);
+    digitalWrite(RESET_OUTPUT, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
   }
   delay(1);
